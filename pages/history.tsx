d73eac72ac1edf72c01fe59c97aeb8634c02b9f6
maciopelo <maciej.k00h@gmail.com>
2024-06-15 17:50:10 +0200

    Add history page

--- pages/history.tsx
+++ pages/history.tsx
@@ -1,9 +1,34 @@
 import Layout from "components/layout";
+import { getPage } from "@/lib/api";
+import { GetStaticProps } from "next";
+import parse from "html-react-parser";
 
-export default function History() {
+interface Props {
+  page: {
+    title: string;
+    content: string;
+  };
+}
+
+export default function History({ page }: Props) {
   return (
     <Layout pageTitle="Historia">
-      <p></p>
+      <div className="p-responsive header-offset flex justify-center">
+        <div className="max-w-screen-lg flex flex-col">
+          <h1 className="font-bold text-2xl pb-4">{page.title}</h1>
+          <article className="history text-justify">
+            {parse(page.content)}
+          </article>
+        </div>
+      </div>
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const page = await getPage(`${process.env.NEXT_PUBLIC_HISTORY_PAGE_ID}`);
+  return {
+    props: { page },
+    revalidate: 10,
+  };
+};
